594568636ad78906941031aa5959f0a8f0b59b94
shiquan <shiquan@tianmtech.cn>
2023-02-07 18:23:10 +0800

    ceshi

---
+++ src/Token.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/*
+ * This file is part of Leonsw.
+ *
+ * @link     https://leonsw.com
+ * @document https://docs.leonsw.com
+ * @contact  leonsw.com@gmail.com
+ * @license  https://leonsw.com/LICENSE
+ */
+namespace TianMiao\JWTAuth;
+
+class Token
+{
+    public $token;
+
+    public $ttl;
+
+    public $iss;
+
+    public function __construct(string $token, int $ttl, string $iss)
+    {
+        $this->token = $token;
+        $this->ttl = $ttl;
+        $this->iss = $iss;
+    }
+}
